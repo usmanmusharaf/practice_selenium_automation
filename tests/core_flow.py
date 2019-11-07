5a07d54eb439c712cc9d3ad4101753c303926d90
usmanmusharaf <usmanmusharaf619@gmail.com>
2019-11-07 14:07:26 +0500

    state_changes

--- tests/core_flow.py
+++ tests/core_flow.py
@@ -46,20 +46,18 @@ class CoreFlow(unittest.TestCase):
         self.driver_summary.select_driver_summary_options()
         self.save_quote.fill_up_save_quote_form('test.carmine@gmail.com', '9999999999', '599 Gresham Ave')
 
-        time.sleep(3)
-
-    def test_flow_in_il_state(self):
-        '''insurify_happy_flow_in_illinois'''
-        self.homepage.enter_zipcode_and_proceed('60007')
-        self.car_make.select_car()
-        self.car_detail.select_car_details()
-        self.driver_info.enter_driver_info('60007')
-        self.discounts.select_discount()
-        self.insured_discount.select_no_prior_insurance()
-        self.professional_discount.enter_professional_information()
-        self.add_incident.add_incident()
-        self.driver_summary.select_driver_summary_options()
-        self.save_quote.fill_up_save_quote_form('test.carmine@gmail.com', '9999999999', '599 Gresham Ave')
+    # def test_flow_in_il_state(self):
+    #     '''insurify_happy_flow_in_illinois'''
+    #     self.homepage.enter_zipcode_and_proceed('60007')
+    #     self.car_make.select_car()
+    #     self.car_detail.select_car_details()
+    #     self.driver_info.enter_driver_info('60007')
+    #     self.discounts.select_discount()
+    #     self.insured_discount.select_no_prior_insurance()
+    #     self.professional_discount.enter_professional_information()
+    #     self.add_incident.add_incident()
+    #     self.driver_summary.select_driver_summary_options()
+    #     self.save_quote.fill_up_save_quote_form('test.carmine@gmail.com', '9999999999', '599 Gresham Ave')
 
 
     def tearDown(self):
